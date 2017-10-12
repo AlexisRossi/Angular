657b69c2c386d6f38faf861f166f5902fc4b0f71
Alexis Jesus Rossi <alexis.rossi@neoris.cxnetworks.net>
2017-10-12 13:47:35 -0300

    Tutorial 2.The Hero Editor

--- tour-of-heroes/src/app/app.component.ts
+++ tour-of-heroes/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+export class Hero{
+  ID : number;
+  name : string;
+}
+
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>ID: </label>{{hero.ID}}</div>
+  <div>
+  <label>name: </label>
+  <input [(ngModel)]= "hero.name" placeholder="name">
+  </div>
+  `,
+  //templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'tour of heroes';
+  hero : Hero = {
+      ID: 1,
+      name: 'Thor' 
+  };
 }
