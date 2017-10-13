29f71c224059ce56a456fcbfb1e6d92f3394fdb5
Alexis Jesus Rossi <alexis.rossi@neoris.cxnetworks.net>
2017-10-13 17:36:05 -0300

    asdsa

--- tour-of-heroes/src/app/app.component.ts
+++ tour-of-heroes/src/app/app.component.ts
@@ -4,26 +4,34 @@ export class Hero{
   ID : number;
   name : string;
 }
-
+const HEROES : Hero[]=[
+  {ID: 11, name: 'Hulk'},
+  {ID: 12, name: 'Thor'},
+  {ID: 13, name: 'Spiderman'},
+  {ID: 14, name: 'Iron Man'},
+  {ID: 15, name: 'Vision'},
+  {ID: 16, name: 'Black Widow'},
+  {ID: 17, name: 'HawkEye'},
+  {ID: 18, name: 'Black Phanter'},
+  {ID: 19, name: 'Ant-Man'},
+  {ID: 20, name: 'Captain America'},
+];
 
 @Component({
   selector: 'app-root',
-  template: `
-  <h1>{{title}}</h1>
-  <h2>{{hero.name}} details!</h2>
-  <div><label>ID: </label>{{hero.ID}}</div>
-  <div>
-  <label>name: </label>
-  <input [(ngModel)]= "hero.name" placeholder="name">
-  </div>
-  `,
-  //templateUrl: './app.component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'tour of heroes';
-  hero : Hero = {
-      ID: 1,
-      name: 'Thor' 
-  };
+  selectedHero : Hero;
+  heroes = HEROES;
+
+  onSelect(hero : Hero){
+    this.selectedHero = hero;
+  }
+
+
+
+
 }
